10ad257ebc3a955e3afd490d91b490abc78b332f
Jacek Chmiel <jacek@chmiel.cc>
2025-10-23 22:32:32 +0200

    Add general options with autogrzybke toggle

--- src/eloelo/options.rs
+++ src/eloelo/options.rs
@@ -1,15 +1,51 @@
-use eloelo_model::options::{DescribedOptionsGroup, Options as _};
+use eloelo_model::options::{DescribedOption, DescribedOptionsGroup, Options};
 use serde::{Deserialize, Serialize};
 use spawelo::SpaweloOptions;
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct GeneralOptions {
+    pub enable_autogrzybke: bool,
+}
+
+impl Default for GeneralOptions {
+    fn default() -> Self {
+        Self {
+            enable_autogrzybke: true,
+        }
+    }
+}
+
+impl Options for GeneralOptions {
+    fn key() -> String {
+        String::from("general")
+    }
+
+    fn name() -> String {
+        String::from("General")
+    }
+
+    fn to_described_options(&self) -> Vec<DescribedOption> {
+        vec![DescribedOption::with_bool(
+            self.enable_autogrzybke,
+            "enableAutogrzybke",
+            "Enable autogrzybke",
+        )]
+    }
+}
 
 #[derive(Clone, Debug, Default, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct EloEloOptions {
+    pub general: GeneralOptions,
     pub spawelo: SpaweloOptions,
 }
 
 impl EloEloOptions {
     pub fn to_described_options_group_vec(&self) -> Vec<DescribedOptionsGroup> {
-        vec![self.spawelo.to_described_options_group()]
+        vec![
+            self.general.to_described_options_group(),
+            self.spawelo.to_described_options_group(),
+        ]
     }
 }
