163b4ac6a77991efdfb1f62cca17b21fc0018b67
Jacek Chmiel <jacek@chmiel.cc>
2025-10-23 22:59:28 +0200

    Use autogrzybke toggle and move fosiaudio call to background

--- src/eloelo/fosiaudio.rs
+++ src/eloelo/fosiaudio.rs
@@ -6,53 +6,6 @@ use log::debug;
 use reqwest::Client;
 use serde::Serialize;
 
-pub async fn call_missing_players<'a>(
-    fosiaudio_host: &str,
-    players_missing_from_lobby: impl Iterator<Item = &'a Player>,
-    timeout: Duration,
-) -> Result<()> {
-    call_autogrzybke(fosiaudio_host, players_missing_from_lobby, timeout, false).await
-}
-
-pub async fn call_single_player<'a>(
-    fosiaudio_host: &str,
-    player: &'a Player,
-    timeout: Duration,
-) -> Result<()> {
-    call_autogrzybke(fosiaudio_host, [player].into_iter(), timeout, true).await
-}
-
-pub async fn announce_winner(
-    fosiaudio_host: &str,
-    winner_team_name: &str,
-    timeout: Duration,
-) -> Result<()> {
-    let winner_team_name = winner_team_name.to_lowercase();
-    debug!("winner_team_name={winner_team_name}");
-    let winner = if winner_team_name.contains("biedronka") {
-        Some("biedronka")
-    } else if winner_team_name.contains("lidl") {
-        Some("lidl")
-    } else {
-        None
-    };
-
-    let Some(winner) = winner else { return Ok(()) };
-    send_autogrzybke_request(
-        fosiaudio_host,
-        AutogrzybkeRequest {
-            missing: winner.to_string(),
-            skip_lobby: true,
-            skip_prefix: true,
-            skip_suffix: true,
-            skip_interlude: true,
-        },
-        timeout,
-    )
-    .await?;
-    Ok(())
-}
-
 #[derive(Serialize)]
 struct AutogrzybkeRequest {
     missing: String,
@@ -62,40 +15,99 @@ struct AutogrzybkeRequest {
     skip_interlude: bool,
 }
 
-async fn send_autogrzybke_request<'a>(
-    fosiaudio_host: &str,
-    request: AutogrzybkeRequest,
+pub struct FosiaudioClient {
+    host: String,
     timeout: Duration,
-) -> Result<()> {
-    let url = format!("http://{fosiaudio_host}/autogrzybke");
-    let _ = Client::new()
-        .post(url)
-        // .body(serde_urlencoded::to_string(fields)?)
-        .body(serde_urlencoded::to_string(request)?)
-        .timeout(timeout)
-        .send()
-        .await?;
-    Ok(())
+    enabled: bool,
 }
 
-async fn call_autogrzybke<'a>(
-    fosiaudio_host: &str,
-    players_missing_from_lobby: impl Iterator<Item = &'a Player>,
-    timeout: Duration,
-    short: bool,
-) -> Result<()> {
-    let player_names: Vec<_> = players_missing_from_lobby
-        .map(|p| p.get_fosiaudio_name())
-        .collect();
-    debug!("Calling {} to lobby", player_names.join(", "));
-    let req = AutogrzybkeRequest {
-        missing: player_names.join("\n"),
-        skip_lobby: short,
-        skip_prefix: false,
-        skip_suffix: false,
-        skip_interlude: false,
-    };
-    send_autogrzybke_request(fosiaudio_host, req, timeout).await?;
-    debug!("Call to lobby sent successfully");
-    Ok(())
+impl FosiaudioClient {
+    pub fn new(host: impl Into<String>) -> Self {
+        Self {
+            host: host.into(),
+            timeout: Duration::from_secs(1),
+            enabled: true,
+        }
+    }
+
+    pub fn with_timeout(mut self, timeout: Duration) -> Self {
+        self.timeout = timeout;
+        self
+    }
+
+    pub fn with_enabled(mut self, enabled: bool) -> Self {
+        self.enabled = enabled;
+        self
+    }
+
+    pub async fn call_missing_players<'a>(
+        &self,
+        players_missing_from_lobby: impl IntoIterator<Item = &'a Player>,
+    ) -> Result<()> {
+        self.call_autogrzybke(players_missing_from_lobby.into_iter(), false)
+            .await
+    }
+    pub async fn call_single_player<'a>(&self, player: &'a Player) -> Result<()> {
+        self.call_autogrzybke([player].into_iter(), true).await
+    }
+
+    pub async fn announce_winner(&self, winner_team_name: &str) -> Result<()> {
+        let winner_team_name = winner_team_name.to_lowercase();
+        debug!("winner_team_name={winner_team_name}");
+        let winner = if winner_team_name.contains("biedronka") {
+            Some("biedronka")
+        } else if winner_team_name.contains("lidl") {
+            Some("lidl")
+        } else {
+            None
+        };
+
+        let Some(winner) = winner else { return Ok(()) };
+        self.send_autogrzybke_request(AutogrzybkeRequest {
+            missing: winner.to_string(),
+            skip_lobby: true,
+            skip_prefix: true,
+            skip_suffix: true,
+            skip_interlude: true,
+        })
+        .await?;
+        Ok(())
+    }
+
+    async fn send_autogrzybke_request<'a>(&self, request: AutogrzybkeRequest) -> Result<()> {
+        if !self.enabled {
+            debug!("Autogrzybke request skipped");
+            return Ok(());
+        }
+        let url = format!("http://{}/autogrzybke", self.host);
+        let _ = Client::new()
+            .post(url)
+            // .body(serde_urlencoded::to_string(fields)?)
+            .body(serde_urlencoded::to_string(request)?)
+            .timeout(self.timeout)
+            .send()
+            .await?;
+        debug!("Autogrzybke request sent successfully");
+        Ok(())
+    }
+
+    async fn call_autogrzybke<'a>(
+        &self,
+        players_missing_from_lobby: impl Iterator<Item = &'a Player>,
+        short: bool,
+    ) -> Result<()> {
+        let player_names: Vec<_> = players_missing_from_lobby
+            .map(|p| p.get_fosiaudio_name())
+            .collect();
+        debug!("Calling {} to lobby", player_names.join(", "));
+        let req = AutogrzybkeRequest {
+            missing: player_names.join("\n"),
+            skip_lobby: short,
+            skip_prefix: false,
+            skip_suffix: false,
+            skip_interlude: false,
+        };
+        self.send_autogrzybke_request(req).await?;
+        Ok(())
+    }
 }
